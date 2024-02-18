b8143694255e01ac08d5e27cf6c864521fd991cb
Ajin Varughese <ajinvarughese91@gmail.com>
2024-02-18 19:12:16 +0530

    #

--- components/ratings/rating.js
+++ components/ratings/rating.js
@@ -4,11 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
     var ratingElement = document.getElementById('rating');
     var descElement = document.getElementById('desc');
 
+    var json;
+
     fetch('./ratingAPI.php')
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            
+            json = data;
             data.forEach(ratingsArray => {
                 ratingsArray.forEach(rating => {
                     prodIDElement.innerHTML += rating.productID + '<br>';
@@ -17,8 +18,32 @@ document.addEventListener('DOMContentLoaded', function() {
                     ratingElement.innerHTML += rating.starCount + '<br>';
                 });
             });
+
+            let totalElements = 0;
+
+            for (let i = 0; i < json.length; i++) {
+                if (Array.isArray(json[i])) {
+                    totalElements += json[i].length;
+                } else {
+                    totalElements++;
+                }
+            }
+    
+            for (let i = 0; i < json.length; i++) {
+                if (Array.isArray(json[i])) {
+                    for (let j = 0; j < json[i].length; j++) {
+                        console.log(json[i][j]);
+                    }
+                } else {
+                    console.log(json[i]);
+                }
+            }
         })
         .catch(error => {
-            console.error('Error fetching data:', error);
+            console.error('Error fetching data');
         });
+
+    function rating(productID) {
+              
+    }
 });
