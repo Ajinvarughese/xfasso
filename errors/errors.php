5a920269749de52282ef4c220bc5716f8ccd80dc
Ajin Varughese <ajinvarughese91@gmail.com>
2024-03-25 06:08:10 +0530

    Create errors.php
    made a page that redirect to errors. it gets error id from url and checkes if I have the error code alredy then redirect to corresponding page.

---
+++ errors/errors.php
@@ -0,0 +1,46 @@
+<?php 
+ 
+   function isExisting(array $arr, $id) {
+      $flag = 0;
+   
+      foreach($arr as $key => $value) {
+         if($id == $value) {
+            $flag = 1;
+            break;
+         }
+      }
+
+      if($flag == 1) {
+         return true;
+      }else {
+         return false;
+      }
+   }
+
+   $existing_errors = 
+   array(
+      "Error" => 1052,
+      "Error2" => 1056
+   );
+ 
+ 
+ if(isset($_GET['errorID'])){
+    
+   $errorID = $_GET['errorID'];
+   
+   if(isExisting($existing_errors, $errorID)) {
+      echo $errorID;
+
+
+      // find what error it is and redirect them to their own pages
+      
+   }else {
+      echo "Error doesn't exist";
+   }
+
+ }else {
+    echo"<script>
+        window.location.href = '../'
+    </script>";
+ }
+?>
