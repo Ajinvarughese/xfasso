be3b6cb18d0cd5e3f453d35d8fc926d360acddc7
Ajin Varughese <ajinvarughese91@gmail.com>
2024-04-08 22:18:34 +0530

    Update query.sql

--- SQL/query.sql
+++ SQL/query.sql
@@ -1,2 +1,19 @@
 DROP DATABASE IF EXISTS xfasso;
 CREATE DATABASE xfasso;
+
+
+
+/* USER TABLE */
+
+CREATE TABLE xfasso.user
+(
+    user_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    username VARCHAR(75),
+    email VARCHAR(255),
+    password VARCHAR(275),
+    gender VARCHAR(8),
+    address LONGTEXT,
+    reg_time DATETIME NOT NULL DEFAULT current_timestamp()
+);
+
+SELECT * FROM xfasso.user
