896ef427ebdada525ceac4ca0fa402f104f1dc9c
Ajin Varughese <ajinvarughese91@gmail.com>
2024-04-09 22:01:16 +0530

    Update query.sql

--- SQL/query.sql
+++ SQL/query.sql
@@ -2,10 +2,7 @@
 DROP DATABASE IF EXISTS xfasso;
 CREATE DATABASE xfasso;
 
-
-
--- USER TABLE 
-
+-- USERS TABLE 
 CREATE TABLE xfasso.users
 (
     user_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -14,15 +11,44 @@ CREATE TABLE xfasso.users
     password VARCHAR(275),
     gender VARCHAR(8),
     address LONGTEXT,
-    reg_time DATETIME NOT NULL DEFAULT current_timestamp()
+    reg_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP()
+);
+
+-- PRODUCTS TABLE
+CREATE TABLE xfasso.products
+(
+    product_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(75),
+    product_price INT,
+    product_image LONGBLOB,
+    product_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    product_gender VARCHAR(10) NOT NULL DEFAULT 'men',
+    rating LONGTEXT,
+    avg_star FLOAT,
+    stock_status TINYINT(1) NOT NULL DEFAULT 0
+);
+
+-- ONE TIME PASSWORD (OTP) TABLE
+
+CREATE TABLE xfasso.otp
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255),
+    otp INT,
+    time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP()
 );
 
 -- CART_USER TABLE
 
+CREATE TABLE xfasso.cart_user
+(
+    SI_NO INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    cart_product INT,
+    FOREIGN KEY (user_id) REFERENCES xfasso.users(user_id),
+    FOREIGN KEY (cart_product) REFERENCES xfasso.products(product_id)
+);
 
--- ONE TIME PASSWORD (OTP) TABLE
+-- Show data from table
+SELECT * FROM xfasso.products;
 
-SELECT * FROM xfasso.users
-
-
-   
