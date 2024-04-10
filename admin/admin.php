9a5d1d659e4fb3ba1f6b1448482f411abf0bdbea
Ajin Varughese <ajinvarughese91@gmail.com>
2024-04-10 21:11:55 +0530

    admin page
    created admin page

---
+++ admin/admin.php
@@ -0,0 +1,84 @@
+<?php 
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADMIN PAGE</title>
+    <link rel="stylesheet" href="style.css">
+
+
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">       
+
+    <style>
+        body {
+            background: #F1EAFF;
+        }
+        .nav {
+            height: 59px;
+            box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 0 3%;
+        }
+        .mainhead{
+            text-align: center;
+            margin-top: 2.5rem;
+        }
+        .head{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .fn213ns{
+            margin-top: 0.6rem;
+            max-width: 72%;
+            opacity: 0.7;
+            text-align: center;
+        }
+        .fn213ns span{
+            font-weight: 600;
+        }
+        .duties {
+            margin-top: 1.5rem;
+            height: 60vh;
+            min-height: 367px;
+            background: #E5D4FF;
+        }
+        @media (max-width: 720px) {
+            .fn213ns{
+                max-width: 100%;
+                padding: 0 5%;
+            }
+        }
+    </style>
+</head>
+<body>
+    <div class="main">
+        <div class="nav">
+            <div class="logo"><h2>XFASSO<h2></div>
+            <div class="admin">ADMIN</div>
+        </div>
+        <div class="header">
+            <div class="head">
+                <h2 class='mainhead'>Choose Your Dutie</h2>
+                <p class='fn213ns'>Choose your dutie from the following options. Make sure all VPNs are turned off. <span>This page is highly confidential</span>. make sure about the safety. Any illegal things you do in this website will lead you to the law.</p>
+            </div>
+            <div class="duties">
+                ajasdn
+            </div>
+        </div>
+    </div>
+</body>
+</html>
