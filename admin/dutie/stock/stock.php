1d638b45571358621996750793a6dec4e5babf47
Ajin Varughese <ajinvarughese91@gmail.com>
2024-04-21 17:35:43 +0530

    final update of stock.php

--- admin/dutie/stock/stock.php
+++ admin/dutie/stock/stock.php
@@ -1,8 +1,41 @@
 <?php 
     session_start();
     require_once '../../../connections/productdb.php';
+    
+    if(empty($_SESSION['XQCLANG'])){
+        header('Location: ../../admin-login/');
+    }
+    
     $quer = "SELECT * FROM products";
     $run = mysqli_query($conn, $quer);
+
+    $upQuer = "SELECT product_id FROM products";
+    $upRun = mysqli_query($conn, $upQuer);
+
+    $prod = array();
+    if(mysqli_num_rows($upRun)>0) {
+        while($upRow= mysqli_fetch_assoc($upRun)) {
+            $prod[] = $upRow['product_id'];
+        }
+    }
+ 
+    if(isset($_POST['submit'])) {
+
+        for($i=0; isset($_POST["prod$i"]); $i++) {
+            $updateProdID = $prod[$i];
+            if($_POST["prod$i"] == 1) { 
+                $updateQuery = "UPDATE products SET stock_status = 1 WHERE product_id = $updateProdID";
+            }else {
+                $updateQuery = "UPDATE products SET stock_status = 0 WHERE product_id = $updateProdID";
+            }
+            try {
+                mysqli_query($conn, $updateQuery);
+                header('Location: ./stock.php');
+            }catch(mysqli_sql_exception) {
+                echo "Change was unsuccessful";
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +44,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stock Management</title>
-    <link rel="stylesheet" href="../../style.css">
+
+
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">       
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <div class="nav">
@@ -45,6 +85,7 @@
 
                 <?php 
                     if(mysqli_num_rows($run)>0) {
+                        $i=0;
                         while($res = mysqli_fetch_assoc($run)) {
 
                             $imageData = base64_encode($res['product_image']);
@@ -63,10 +104,11 @@
                                     <td class='prodName'>{$prodName}</td>
                                     <td class='prodPrice'>{$prodPrice}</td>
                                     <td class='stock'>
-                                        <input name='' type='text' style='padding: 2px; width:40px; text-align: center; font-size:15px;' value='{$prodStock}' >
+                                        <input name='prod$i' type='text' style='padding: 2px; width:40px; text-align: center; font-size:15px;' value='{$prodStock}' >
                                     </td>
                                 </tr>
                             ";
+                            $i++;
                         }
                     }
                 ?>
@@ -74,9 +116,10 @@
             </table>
             <div class="sub" class="final">
                 <p style="font-size: 13px;">Make sure before you submit the changes: </p>    
-                <input type="submit">
+                <input type="submit" name="submit">
             </div>
         </form>
     </div>
 </body>
+<script src="../main.js"></script>
 </html>
