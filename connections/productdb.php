e9882dd465cc98ac0402d0ff6900850ff7970ab0
Ajin Varughese <ajinvarughese91@gmail.com>
2024-05-09 20:13:29 +0530

    update

--- connections/productdb.php
+++ connections/productdb.php
@@ -1,38 +1,20 @@
 <?php 
-    require __DIR__ . '/../vendor/autoload.php'; 
-    use Dotenv\Dotenv;
 
-    $dotenv = Dotenv::createImmutable(__DIR__.'/../');
-    $dotenv->load();
-
-    $server = $_ENV['DB_HOST'] ?? '';
-    $user = $_ENV['DB_USER'] ?? '';
-    $password = $_ENV['DB_PASSWORD'] ?? '';
-    $db = $_ENV['DB_NAME'] ?? '';
+    $server = 'localhost';
+    $user = 'root';
+    $password = '';
+    $db = 'xfasso';
 
     
-    if ($server && $user && $db) {
+    try {
         $conn = new mysqli($server, $user, $password, $db);
-
-        if ($conn->connect_error) {
-            echo "
-                <script>
-                    window.location.href = '../errors/errors.php?errorID=1025';
-                </script>
-            ";
-            die();
-        }
-        if (file_exists('.env')) {
-            unlink('.env');
-        }
-    } else {
-        
-       echo "
+    }catch(mysqli_sql_exception) {
+        echo "
             <script>
                 window.location.href = '../errors/errors.php?errorID=1025';
             </script>
         ";
+        die();
     }
-
 
 ?>
